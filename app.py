fc99c2ad0c5d4e07be6bd715b444a3b6a226cb0d
jack <2913895+tiga1231@users.noreply.github.com>
2023-03-27 10:51:14 -0500

    smoothness constraint init

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 import numpy as np
 import pandas as pd
 
+from textwrap import dedent
 from base64 import b64encode
 # from tqdm import tqdm
 
@@ -136,7 +137,140 @@ f1 = {f1}
             ))
     for p in predicates:
         print(p)
-    return predicates, mu, a, accuracy, precision, recall, f1
+    return predicates, mu, a, [accuracy, precision, recall, f1]
+
+
+def compute_predicate_sequence(x0, selected, n_iter=1000):
+    '''
+        x0 - numpy array, shape=[n_points, n_feature]. Data points
+        selected - boolean array. shape=[brush_index, n_points] of selection
+    '''
+    mu_init=None
+    a_init=0.4
+
+    n_points, n_features = x0.shape
+    n_brushes = selected.shape[0]
+
+    # prepare training data
+    # orginal data extent
+    vmin = x0.min(0)
+    vmax = x0.max(0)
+    x = torch.from_numpy(x0.astype(np.float32))
+    label = torch.from_numpy(selected).float()
+    # normalize
+    mean = x.mean(0)
+    scale = x.std(0) + 0.1
+    x = (x - mean) / scale
+
+    # Trainable parameters
+    # since data is normalized,
+    # mu can initialized around mean_pos examples
+    # a can initialized around a constant across all axes
+    selection_centroids = torch.stack([x[sel_t].mean(0) for sel_t in selected], 0)
+
+    if mu_init is None:
+        mu_init = selection_centroids
+    a = (a_init + 0.1*(2*torch.rand(n_brushes, n_features)-1))
+    mu = mu_init + 0.1 * (2*torch.rand(n_brushes, x.shape[1]) - 1)
+    a.requires_grad_(True)
+    mu.requires_grad_(True)
+
+    # weight-balance selected vs. unselected based on their size
+    bce_per_brush = []
+    for st in selected: ## for each brush, define their class-balanced loss function
+        n_selected = st.sum()
+        n_unselected = n_points - n_selected
+        instance_weight = torch.ones(x.shape[0])
+        instance_weight[st] = n_points/n_selected
+        instance_weight[~st] = n_points/n_unselected
+        bce = nn.BCELoss(weight=instance_weight)
+        bce_per_brush.append(bce)
+
+
+    optimizer = optim.SGD([
+        {'params': mu, 'weight_decay': 0},
+        # smaller a encourages larger reach of the bounding box
+        {'params': a, 'weight_decay': 0.01}
+    ], lr=1e-2, momentum=0.9)
+
+    # training loop
+    for e in range(n_iter):
+        loss_per_brush = []
+        for t, st in enumerate(selected): ## for each brush, compute loss
+            pred = predict(x, a[t], mu[t])
+            loss = bce(pred, label[t])
+            loss += (mu[t] - selection_centroids[t]).pow(2).mean() * 20
+            loss_per_brush.append(loss)
+        smoothness_loss = 100 * (a[1:]-a[:-1]).pow(2).mean()
+        smoothness_loss += 100 * (mu[1:]-mu[:-1]).pow(2).mean()
+
+        total_loss = sum(loss_per_brush) + smoothness_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+        if e % (n_iter//5) == 0:
+            # print(pred.min().item(), pred.max().item())
+            print(f'[{e:>4}] loss {loss.item()}')
+    a.detach_()
+    mu.detach_()
+    # plt.stem(a.abs().numpy()); plt.show()
+
+    qualities = []
+    for t, st in enumerate(selected): ## for each brush, compute loss
+        pred = predict(x, a[t], mu[t])
+        pred = (pred > 0.5).float()
+        correct = (pred == label[t]).float().sum().item()
+        total = n_points
+        accuracy = correct/total
+        # 1 meaning points are selected
+        tp = ((pred == 1).float() * (label == 1).float()).sum().item()
+        fp = ((pred == 1).float() * (label == 0).float()).sum().item()
+        fn = ((pred == 0).float() * (label == 1).float()).sum().item()
+        precision = tp/(tp+fp) if tp+fp>0 else 0
+        recall = tp/(tp+fn) if tp+fn>0 else 0
+        f1 = 1/(1/precision + 1/recall) if precision>0 and recall>0 else 0
+        print(dedent(f'''
+            brush = {t}
+            accuracy = {accuracy}
+            precision = {precision}
+            recall = {recall}
+            f1 = {f1}
+        '''))
+        qualities.append(dict(
+            brush = t,
+            accuracy = accuracy,
+            precision = precision,
+            recall = recall,
+            f1 = f1
+        ))
+
+    # predicate clause selection
+    # r is the range of the bounding box on each dimension
+    # bounding box is defined by the level set of prediction=0.5
+    predicates = []
+    for t, st in enumerate(selected): ## for each brush, generate a predicate from a[t] and mu[t]
+        r = 1 / a[t].abs()
+        predicate_clauses = []
+        for k in range(n_features): # for each attribute
+            # denormalize
+            r_k = (r[k] * scale[k]).item()
+            mu_k = (mu[t,k] * scale[k] + mean[k]).item()
+            ci = [mu_k - r_k, mu_k + r_k]
+            assert ci[0] < ci[1], 'ci[0] is not less than ci[1]'
+            if ci[0] < vmin[k]:
+                ci[0] = vmin[k]
+            if ci[1] > vmax[k]:
+                ci[1] = vmax[k]
+            # feature selection based on extent range
+    #         should_include = r[k] < 1.0 * (x[:,k].max()-x[:,k].min())
+            should_include = not (ci[0] <= vmin[k] and ci[1] >= vmax[k])
+            if should_include:
+                predicate_clauses.append(dict(
+                    dim=k, interval=ci
+                ))
+        predicates.append(predicate_clauses)
+    parameters = dict(mu=mu, a=a)
+    return predicates, qualities, parameters
 
 
 current_dataset = None
@@ -155,28 +289,34 @@ def get_predicate():
                 df = df.drop(attr, axis='columns')
         x0 = df.to_numpy()
         current_dataset = dataset
-
     # a sequence of bool arrays indexed by [brush time, data point index]
     subsets = np.array(request.json['subsets'])
 
-    predicates = []
-    quality_measures = []
-    mu = None
-    a = 0.4
-    for subset in subsets:
-        predicate, mu, a, accuracy, precision, recall, f1 = compute_predicate(
-            x0, subset, mu_init=mu, a_init=a)
-        predicates.append(predicate)
-        quality_measures.append(dict(
-            accuracy=accuracy,
-            precision=precision,
-            recall=recall,
-            f1=f1,
-        ))
+    # # Option 1: each brush is indepedently optimized through compute_predicate()
+    # predicates = []
+    # qualities = []
+    # mu = None
+    # a = 0.4
+    # for t, subset in enumerate(subsets):
+    #     predicate, mu, a, [accuracy, precision, recall, f1] = compute_predicate(
+    #         x0, subset, mu_init=mu, a_init=a)
+    #     predicates.append(predicate)
+    #     qualities.append(dict(
+    #         brush=t,
+    #         accuracy=accuracy,
+    #         precision=precision,
+    #         recall=recall,
+    #         f1=f1,
+    #     ))
+
+    # Option 2: jointly optimize the sequence
+    predicates, qualities, parameters = compute_predicate_sequence(x0, subsets, n_iter=1000)
+
+
     # TODO compute and report precision recall accuracy and  F1
     return dict(
         predicates=predicates,
-        quality_measures=quality_measures,
+        qualities=qualities,
     )
 
 
